af4bcccd057c6f190b4477465dcecd915532cad0
Adam Więcek <adamw28@gmail.com>
2017-12-27 01:39:57 +0100

    End project

---
+++ program.js
@@ -0,0 +1,26 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    process.stdout.write('version - podaje wersję node.js\nlang - podaje język systemowy \n/exit - wyjście');
+    if(input !== null) {
+        // teraz jest sens cokolwiek wyświetlać :)
+        var instruction = input.toString().trim();
+        switch (instruction) {
+        	case '/exit': {
+	        	process.stdout.write('Quitting app!\n');
+    	    	process.exit();
+        	}
+        	case 'version': {
+            	console.log(process.version);
+            	break;
+        	}
+        	case 'lang': {
+            	console.log(process.env.lang);
+            	break;
+            }
+        	default:
+            	process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
